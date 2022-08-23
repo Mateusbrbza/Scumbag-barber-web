d3129e4755673dc3ddc58f07159c565870c73b00
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-08-23 20:05:16 -0300

    inicio

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Route, Switch } from 'react-router-dom';
+
+import Login from './components/login-screen/Login';
+
+function App() {
+    return (
+        <Login />
+        
+    )
+}
+
+export default App;
