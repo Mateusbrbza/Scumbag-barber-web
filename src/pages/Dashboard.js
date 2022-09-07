25bac97fc033ddc8beab3d215af2be40ef492564
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-09-07 12:55:36 -0300

    .

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { AiFillDollarCircle } from "react-icons/ai";
+
+import Button from '../components/Button';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Dashboard = () => {
+  const {currentColor} = useStateContext();
+
   return (
     <div>Dashboard</div>
   )
